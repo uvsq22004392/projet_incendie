aaa56cc759229a71453947851f144928b8ef5e8d
uvsq22000761 <72154094+uvsq22000761@users.noreply.github.com>
2021-02-16 12:01:18 +0100

    ajout de fonctions

--- incendie.py
+++ incendie.py
@@ -4,6 +4,28 @@
 # Andrew BAYISSA
 # Joris ANGAMAN
 # Martin DIAMANT
-# Amel LASSAN
+# Amel LASSAL
 # Thomas TURPIN
 # https://github.com/uvsq22004392/projet_incendie
+######################################
+
+import random as rd
+
+def etat_foret(nf):
+    proba = nf * 0.1 * 100
+    feu = rd.randint(0,100)
+    if feu < proba:
+        return "feu"
+    
+def terrain():
+    a = rd.randint(0,2)
+    if a == 0:
+        return "foret"
+    if a == 1:
+        return "prairie"
+    if a == 2:
+        return "eau"
+
+cases = [[[terrain()] for i in range(50)] for i in range(50)]
+
+print(cases)

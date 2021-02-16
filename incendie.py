9381406592e0966e5c011dbef2fff4b931d8d247
Lenny BACKORY <lenny.backory@ens.uvsq.fr>
2021-02-16 10:27:00 +0100

    names

---
+++ incendie.py
@@ -0,0 +1,9 @@
+######################################
+# Groupe BI 2
+# Lenny BACKORY
+# Andrew BAYISSA
+# Joris ANGAMAN
+# Martin DIAMANT
+# Amel LASSAN
+# Thomas TURPIN
+# https://github.com/uvsq22004392/projet_incendie
